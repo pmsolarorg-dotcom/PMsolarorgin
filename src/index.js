aa4d8b83d93596fe02d739b6543a0eca8a847591
pmsolarorg-dotcom <pmsolar.org@gmail.com>
2025-09-08 23:14:14 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// src/index.js
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css"; // optional - create if you want
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<React.StrictMode><App /></React.StrictMode>);
